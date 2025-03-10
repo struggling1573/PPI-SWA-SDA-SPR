cf23aa531c422fd52b5dd81ad58855cee6132277
haoyu Wang <wanghyx666@163.com>
2025-03-10 20:15:25 +0800

    Create run_orthofinder.sh

---
+++ Phylogenomics/run_orthofinder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=orthofinder
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=wanghyx666@163.com
+#SBATCH --output=job-%j.out
+#SBATCH --error=job-%j.err
+
+
+source /biosoft/miniconda3/etc/profile.d/conda.sh
+conda activate orthofinder
+
+orthofinder -f pep_rename
