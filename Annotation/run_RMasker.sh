360497758a25647a1ad0d1f738e6fe5307850e48
haoyu Wang <wanghyx666@163.com>
2025-03-10 19:53:12 +0800

    Create run_RMasker.sh

---
+++ Annotation/run_RMasker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=RMasker
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=wanghyx666@163.com
+#SBATCH --output=job-%j.out
+#SBATCH --error=job-%j.err
+
+REF=/path_to_data/Percocypris_pingi.genomic.fna
+repeate_lib=/path_to_data/Percocypris_pingi.RM.lib
+prefix=Percocypris_pingi
+
+export PATH=/home/bio_soft/RM/RepeatMasker-4.1.2-p1:$PATH
+export BLAST_USAGE_REPORT=false
+
+RepeatMasker -pa 4 -gff -a -xsmall -gccalc -dir ${prefix}_RM.output -lib $repeate_lib $REF
