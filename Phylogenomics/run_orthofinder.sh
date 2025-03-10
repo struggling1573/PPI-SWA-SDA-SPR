a8aeff5eb6de7c5c02f8f0538cd29930d1fac59a
haoyu Wang <wanghyx666@163.com>
2025-03-10 20:17:20 +0800

    Update run_orthofinder.sh

--- Phylogenomics/run_orthofinder.sh
+++ Phylogenomics/run_orthofinder.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=orthofinder
 #SBATCH --partition=cpu
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=wanghyx666@163.com
@@ -13,4 +13,4 @@
 source /biosoft/miniconda3/etc/profile.d/conda.sh
 conda activate orthofinder
 
-orthofinder -f pep_rename
+orthofinder -f pep_rename -t 8 -a 8 -S blast -og
