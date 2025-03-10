b3d965028affce04499d1041343ce6319dfb69a2
haoyu Wang <wanghyx666@163.com>
2025-03-10 19:49:40 +0800

    Create run_RModeler.sh

---
+++ Annotation/run_RModeler.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=RModeler
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=20G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=wanghyx666@163.com
+#SBATCH --output=job-%j.out
+#SBATCH --error=job-%j.err
+
+REF=/path_to_data/Percocypris_pingi.genomic.fna
+db=PPI_db
+prefix=Percocypris_pingi
+export PATH=/home/bio_soft/RM/RepeatModeler-2.0.3:$PATH
+export BLAST_USAGE_REPORT=false
+
+mkdir $db
+BuildDatabase -name $db/$db $REF
+RepeatModeler -database $db/$db -pa 4
+less $db/${db}-families.fa |sed "s/rnd/${prefix}_rnd/g" | sed 's/ .*//g' >${prefix}.RModeler.fa
+grep '>' $db/${db}-families.fa |sed "s/rnd/${prefix}_rnd/g" |sed 's/>//g' | sed 's/ (/\t(/g' >${prefix}.RModeler.anno
