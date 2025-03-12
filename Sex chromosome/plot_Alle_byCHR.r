507f15bc9896e8153269cdf5750426c7d83f2f2f
haoyu Wang <wanghyx666@163.com>
2025-03-12 16:12:05 +0800

    Update plot_Alle_byCHR.r

--- Sex chromosome/plot_Alle_byCHR.r
+++ Sex chromosome/plot_Alle_byCHR.r
@@ -1,32 +1,46 @@
-# Usage: Rscript plot.r qk-11-mu.gene_num.count qk-11-mu.gene_num.count.pdf qk-11-mu
-# 获取输入和输出文件路径
-input<- commandArgs(trailingOnly = TRUE)[1]
-output<- commandArgs(trailingOnly = TRUE)[2]
-sample<- commandArgs(trailingOnly = TRUE)[3]
+print_help <- function() {
+    cat("
+This script is used to plot a percentage stacked bar chart based on allele count data.
 
-# Load Package
+Usage: Rscript plot.r <input> <output> <sample> 
+
+Arguments:
+  <input>    The input file containing allele count data with a header.
+  <output>   The output file path for saving the generated plot.
+  <sample>   The sample name used in the plot title.
+
+Author: Hangyu Wang
+Date: Dec 29 2024
+Unit: Southwest University
+Contact: wanghyx666@163.com
+")
+}
+
+args <- commandArgs()
+if ("-h" %in% args) {
+    print_help()
+    q(save = "no")
+}
+
+if (length(commandArgs(trailingOnly = TRUE)) < 3) {
+    stop("Please provide both input and output file paths and sample name. Use -h for help.")
+}
+
+input <- commandArgs(trailingOnly = TRUE)[1]
+output <- commandArgs(trailingOnly = TRUE)[2]
+sample <- commandArgs(trailingOnly = TRUE)[3]
+
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 
-# 生成示例数据
 data <- read.table(input, header = T)
-
-# 将数据转换为长格式
 data_long <- data %>%
   gather(key = "Type", value = "Value", -CHR)
-head(data_long)
-
 data_filtered <- data_long %>% filter(.[[2]]!= "AlleNumber" & .[[2]]!= "Orther")
-head(data_filtered)
-
-# 计算百分比
 data_filtered <- data_filtered %>%
   group_by(CHR) %>%
   mutate(Percentage = Value / sum(Value) * 100)
-head(data_filtered)
-
-# 绘制百分比堆积柱状图
 plot_title <- paste("Genotypic statistics in ", sample, sep = "")
 ggplot(data_filtered, aes(x=CHR, y=Percentage, fill=Type)) +
   geom_bar(stat="identity") +
@@ -40,4 +54,4 @@ ggplot(data_filtered, aes(x=CHR, y=Percentage, fill=Type)) +
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank()
   )
-ggsave(output, width = 5, height = 3)
+ggsave(output, width = 5, height = 3)    
